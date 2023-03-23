92c537c983c4fe38e9977d35685d054d1d102850
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-03-23 17:07:19 +0700

    Add the example of Heap

---
+++ heap/heap/heap.go
@@ -0,0 +1,50 @@
+package heap
+
+type Heap struct {
+	values []int
+}
+
+func New() *Heap {
+	return &Heap{values: []int{0}}
+}
+
+func (h *Heap) Push(v int) {
+	h.values = append(h.values, v)
+	i := len(h.values) - 1
+
+	for h.values[i] < h.values[i/2] {
+		h.values[i], h.values[i/2] = h.values[i/2], h.values[i]
+		i /= 2
+	}
+}
+
+func (h *Heap) Pop() int {
+	if len(h.values) == 1 {
+		return 0
+	}
+
+	if len(h.values) == 2 {
+		return h.Pop()
+	}
+
+	res := h.values[1]
+	// Move last value to root
+	h.values[1] = h.Pop()
+	i := 1
+
+	for 2*i < len(h.values) {
+		if 2*i+1 < len(h.values) && h.values[2*i+1] < h.values[2*i] && h.values[i] > h.values[2*i+1] {
+			// Swap right child
+			h.values[i], h.values[2*i+1] = h.values[2*i+1], h.values[i]
+			i = 2*i + 1
+		} else if h.values[i] > h.values[2*i] {
+			// Swap left child
+			h.values[i], h.values[2*i] = h.values[2*i], h.values[i]
+			i = 2 * i
+		} else {
+			break
+		}
+	}
+
+	return res
+}
