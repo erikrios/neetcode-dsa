300dbfbecf285ab0fa5f6332a709cf564fb738fa
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-03-23 17:25:47 +0700

    Add the example of Heap

--- heap/heap/heap.go
+++ heap/heap/heap.go
@@ -48,3 +48,28 @@ func (h *Heap) Pop() int {
 
 	return res
 }
+
+func (h *Heap) Heapify(nums []int) {
+	// 0-th position is moved to the end
+	nums = append(nums, nums[0])
+
+	h.values = nums
+	cur := len(h.values) - 1/2
+
+	for cur > 0 {
+		i := cur
+		if 2*i+1 < len(h.values) && h.values[2*i+1] < h.values[2*i] && h.values[i] > h.values[2*i+1] {
+			// Swap right child
+			h.values[i], h.values[2*i+1] = h.values[2*i+1], h.values[i]
+			i = 2*i + 1
+		} else if h.values[i] > h.values[2*i] {
+			// Swap left child
+			h.values[i], h.values[2*i] = h.values[2*i], h.values[i]
+			i = 2 * i
+		} else {
+			break
+		}
+	}
+
+	cur--
+}
